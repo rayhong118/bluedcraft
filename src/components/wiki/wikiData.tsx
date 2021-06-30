f546caeb6dc6807c62f5fdff049144989aab72c3
doghead <rayhong118@gmail.com>
2021-06-29 22:28:11 -0700

    builder application info added to wiki

--- src/components/wiki/wikiData.tsx
+++ src/components/wiki/wikiData.tsx
@@ -22,7 +22,22 @@ export const WikiData: WikiArticle[] = [
   },
   { title: "官方收购价格", description: "", content: [] },
   {
-    title: "基础指令", description: "", content: [
+    title: "建筑师考核",
+    description: "关于建筑师等级和各级考核要求",
+    content: [
+      "初级考核：",
+      "现时一小时内，建造下列建筑，风格不限但最好是现代，最好有内部大致布局，允许缩小比例：",
+      "中密度建筑（办公楼，公寓楼，混合用途公寓*，医院，商场等高度不高于10层）",
+      "低密度建筑（连体别墅，独栋别墅，商场，商店等）",
+      "*：底层为商户，上面是公寓",
+      "中级考核：",
+      "不限时间完成初级考核涉及到的建筑",
+    ],
+  },
+  {
+    title: "基础指令",
+    description: "",
+    content: [
       "传送",
       "/wp 打开传送面板-仅限于平川及洛卡使用",
       "/tpa 需加入城镇",
@@ -31,63 +46,56 @@ export const WikiData: WikiArticle[] = [
       "经济",
       "/bdeco create 创建银行账户（没有账号无法完成货币交易）",
       "/bdeco pay 转账",
-    ]
+    ],
   },
   {
-    title: "城镇：洛卡", description: "", content: [
-      "加入条件: 280,000LKD",
-      "简介",
-    ]
+    title: "城镇：洛卡",
+    description: "",
+    content: ["加入条件: 280,000LKD", "简介"],
   },
   {
-    title: "城镇：羊坊", description: "", content: [
-      "加入条件: 邀请",
-      "简介",
-    ]
+    title: "城镇：羊坊",
+    description: "",
+    content: ["加入条件: 邀请", "简介"],
   },
   {
-    title: "城镇：时雨", description: "", content: [
-      "加入条件: 城镇无了",
-      "简介",
-    ]
+    title: "城镇：时雨",
+    description: "",
+    content: ["加入条件: 城镇无了", "简介"],
   },
   {
-    title: "城镇：雪城", description: "", content: [
-      "加入条件: 平川NPC",
-      "城主：Aluminum_Z",
-      "建筑限制：",
-      "简介",
-    ]
+    title: "城镇：雪城",
+    description: "",
+    content: ["加入条件: 平川NPC", "城主：Aluminum_Z", "建筑限制：", "简介"],
   },
   {
-    title: "城镇：蓝星", description: "", content: [
+    title: "城镇：蓝星",
+    description: "",
+    content: [
       "加入条件: 平川NPC",
       "城主：Cloud_luo",
       "建筑限制：主城区内用地需审批。整体建筑风格保持一致",
       "简介",
-    ]
+    ],
   },
   {
-    title: "城镇：彩云", description: "", content: [
-      "加入条件: 平川NPC",
-      "城主：Truly_false",
-      "建筑限制：",
-      "简介",
-    ]
+    title: "城镇：彩云",
+    description: "",
+    content: ["加入条件: 平川NPC", "城主：Truly_false", "建筑限制：", "简介"],
   },
   {
-    title: "卡片", description: "", content: [
-      "访问 http://help.bluedcraft.com/plugin/",
-    ]
+    title: "卡片",
+    description: "",
+    content: ["访问 http://help.bluedcraft.com/plugin/"],
   },
   {
-    title: "烹饪", description: "", content: [
-      "访问 http://help.bluedcraft.com/plugin/",
-    ]
+    title: "烹饪",
+    description: "",
+    content: ["访问 http://help.bluedcraft.com/plugin/"],
   },
   {
-    title: "载具", description: "", content: [
-      "访问 http://help.bluedcraft.com/plugin/",
-    ]
+    title: "载具",
+    description: "",
+    content: ["访问 http://help.bluedcraft.com/plugin/"],
   },
 ];
