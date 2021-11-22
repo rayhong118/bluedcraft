2c7debc4b04c5b2a9a8620c403536426efe70a95
233Chad <27846309+233Chad@users.noreply.github.com>
2021-11-22 11:40:36 +0800

    wiki content update

--- src/components/wiki/articleList.tsx
+++ src/components/wiki/articleList.tsx
@@ -22,47 +22,11 @@ export const WikiData: WikiArticle[] = [
     description: "",
     content: `",`,
   },
-  // {
-  //   id: 0,
-  //   title: "城镇：洛卡",
-  //   description: "",
-  //   content: ["加入条件: 280,000LKD", "简介"],
-  // },
-  // {
-  //   id: 0,
-  //   title: "城镇：羊坊",
-  //   description: "",
-  //   content: ["加入条件: 邀请", "简介"],
-  // },
-  // {
-  //   id: 0,
-  //   title: "城镇：时雨",
-  //   description: "",
-  //   content: ["加入条件: 城镇无了", "简介"],
-  // },
-  // {
-  //   id: 0,
-  //   title: "城镇：雪城",
-  //   description: "",
-  //   content: ["加入条件: 平川NPC", "城主：Aluminum_Z", "建筑限制：", "简介"],
-  // },
-  // {
-  //   id: 0,
-  //   title: "城镇：蓝星",
-  //   description: "",
-  //   content: [
-  //     "加入条件: 平川NPC",
-  //     "城主：Cloud_luo",
-  //     "建筑限制：主城区内用地需审批。整体建筑风格保持一致",
-  //     "简介",
-  //   ],
-  // },
-  // {
-  //   id: 0,
-  //   title: "城镇：彩云",
-  //   description: "",
-  //   content: ["加入条件: 平川NPC", "城主：Truly_false", "建筑限制：", "简介"],
-  // },
+  {
+    id: 4,
+    title: "城镇",
+    description: "城镇详细介绍",
+  },
   // {
   //   id: 0,
   //   title: "卡片",
