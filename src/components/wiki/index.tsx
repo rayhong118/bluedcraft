2a0bc7e8b05052887a035fd8266b45a0afe1c8ff
233Chad <27846309+233Chad@users.noreply.github.com>
2021-11-23 23:29:27 +0800

    fixed layout

--- src/components/wiki/index.tsx
+++ src/components/wiki/index.tsx
@@ -4,44 +4,17 @@ import { WikiData } from "./articleList";
 import { Stack, IStackItemStyles, IStackStyles } from '@fluentui/react';
 import "./wiki.scss";
 
-const stackItemStyles: IStackItemStyles = {
-  root: {
-    maxWidth: 300,
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    overflow: 'hidden',
-  },
-};
-
-const nonShrinkingStackItemStyles: IStackItemStyles = {
-  root: {
-    alignItems: 'center',
-    display: 'flex',
-    justifyContent: 'center',
-    overflow: 'hidden',
-  },
-};
-
-const stackStyles: IStackStyles = {
-  root: {
-    overflow: 'hidden',
-    width: `100%`,
-  },
-};
 
 const WikiComponent = () => {
   return (
-      // TODO: resize
-    <div className="wiki-page page">
-      <Stack horizontal styles={stackStyles}>
-        <Stack.Item grow styles={stackItemStyles}>
-          <WikiNav listOfArticles={WikiData} />
-        </Stack.Item>
-        <Stack.Item grow disableShrink styles={nonShrinkingStackItemStyles} >
-          <Wiki />
-        </Stack.Item>
-      </Stack>
+    // TODO: resize
+    <div className="wiki-page">
+      <div className="wiki-nav">
+        <WikiNav listOfArticles={WikiData} />
+      </div>
+      <div className="wiki-list">
+        <Wiki />
+      </div>
     </div>
   );
 };
