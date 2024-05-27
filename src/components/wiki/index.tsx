851ee79bd03351412e758258863056b3f5b962dc
doghead <rayhong118@gmail.com>
2024-05-27 14:19:57 -0700

    update wiki navigation

--- src/components/wiki/index.tsx
+++ src/components/wiki/index.tsx
@@ -3,18 +3,41 @@ import { WikiNav } from "./nav";
 import "./wiki.scss";
 import { useEffect, useState } from "react";
 import { WikiContext } from "./context";
+import { useBoolean } from "@fluentui/react-hooks";
+import { DefaultButton, Panel } from "@fluentui/react";
 
 const WikiComponent = () => {
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
   const [selectedArticleId, setSelectedArticleId] = useState("");
+  const [isOpen, { setTrue: openPanel, setFalse: dismissPanel }] =
+    useBoolean(false);
+  useEffect(() => {
+    dismissPanel();
+    console.log(selectedArticleId);
+  }, [selectedArticleId]);
 
   return (
     <WikiContext.Provider value={{ selectedArticleId, setSelectedArticleId }}>
       <div className="page adaptive-margin">
         <h2>Wiki 百科</h2>
-        <div className="wiki-page ">
+        <DefaultButton
+          className="nav-panel-button"
+          text="目录"
+          onClick={openPanel}
+        />
+        <div className="wiki-page">
+          <Panel
+            className="nav-panel"
+            isLightDismiss
+            isOpen={isOpen}
+            onDismiss={dismissPanel}
+            closeButtonAriaLabel="Close"
+            headerText="百科目录"
+          >
+            <WikiNav />
+          </Panel>
           <WikiNav />
           <Wiki />
         </div>
--- src/components/wiki/wiki.tsx
+++ src/components/wiki/wiki.tsx
@@ -1,26 +1,12 @@
-import { useNavigate } from "react-router-dom";
 import { Article } from "./article";
-import { DefaultButton } from "@fluentui/react/lib/Button";
-import { IIconProps } from "@fluentui/react/lib/Icon";
 
 export interface IWikiContext {
   selectedArticleId: number;
 }
 
 export const Wiki = () => {
-  const navigate = useNavigate();
-  const faAngleLeft: IIconProps = { iconName: "back" };
-
   return (
     <div className="wiki-container wiki-list">
-      <DefaultButton
-        iconProps={faAngleLeft}
-        text="返回"
-        onClick={() => {
-          navigate(-1);
-        }}
-        allowDisabledFocus
-      />
       <Article />
     </div>
   );
